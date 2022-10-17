c028427337b329a3162f5dfec5f0fd046eab9553
stephen <nwabuezesc@gmail.com>
2022-10-17 21:45:12 +0100

    making some changes in material UI theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,35 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Navbar, Footer } from './Components';
 import { Home, About, Events, Gallery, Sermon } from './Pages';
+import { createTheme, ThemeProvider} from '@mui/material';
 
 import './App.css';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4D0074 '
+    },
+    secondary: {
+      main: '#AC7F34'
+    }
+  }
+})
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/About" element={<About/>} />
-        <Route path="/Event" element={<Events/>} />
-        <Route path="/Gallery" element={<Gallery/>} />
-        <Route path="/Sermon" element={<Sermon/>} />
-      </Routes>
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/Event" element={<Events/>} />
+          <Route path="/Gallery" element={<Gallery/>} />
+          <Route path="/Sermon" element={<Sermon/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 // import images from '../../assets';
+import { Typography, Box, Button } from '@mui/material';
+import styled from '@emotion/styled';
 import './Home.css'
 
+const useStyles = styled(theme => ({
+  hero_heading:{
+    paddingTop: 250,
+    padding: 140,
+    color: 'white',
+    width: 900,
+    marginTop: 195
+  }
+}))
+
 const Header = () => {
+  const classes = useStyles();
+
   return (
     // Adding the Hero section
-    <section className='hero_background_img'>
+    <Box className='hero_background_img'>
       <div className='overlay'></div>
-      <div className='hero_heading'>
-        THE ANGLICAN CHURCH OF THE MESSIAH
-      </div>
-      <div className='hero_body'>
-        <p>Come join us at the Church of the Messiah where Jesus the Messiah will meet your needs.</p>
-      </div>
-    </section>
+      <Box className={classes.hero_heading}>
+        <Typography variant="h2" >THE ANGLICAN CHURCH OF THE MESSIAH</Typography>
+      </Box>
+      <Box className='hero_body'>
+        <Typography variant='p'>Come join us at the Church of the Messiah where Jesus the Messiah will meet your needs.</Typography>
+      </Box>
+      <Button className='btn' variant="contained">worship with us</Button>
+    </Box>
   )
 }
 
